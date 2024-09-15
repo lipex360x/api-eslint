1ebeebd2ffd41974ba410384728935149db68b96
Felipe Bambirra <lipex360@gmail.com>
2024-09-15 11:41:16 +0100

    create user added

---
+++ tests/integration/create-user.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from 'bun:test'
+
+import { CreateUserUseCase } from '@/application/usecases'
+
+describe('INT', () => {
+  test('create user', async () => {
+    const input = {
+      name: 'john doe',
+      email: 'john@mail.com',
+    }
+    const usecase = new CreateUserUseCase()
+    const output = await usecase.execute(input)
+    expect(output).toEqual({
+      name: 'john doe',
+      email: 'john@mail.com',
+    })
+  })
+})

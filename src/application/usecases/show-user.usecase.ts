c17ca2a6774263ec02ac72ce36a24b7550fc59be
Felipe Bambirra <lipex360@gmail.com>
2024-09-15 13:54:16 +0100

    show user updated

--- src/application/usecases/show-user.usecase.ts
+++ src/application/usecases/show-user.usecase.ts
@@ -1,5 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { User } from '@/domain/entities'
+import type { UserRepository } from '@/infra/repositories'
+
 export class ShowUserUseCase {
-  async execute() {
-    return 'Hello World!'
+  constructor(private readonly repository: UserRepository) {}
+
+  async execute(userId: string) {
+    const getUser: any = this.repository.database.find((data: any) => data.userId === userId)
+    const user = new User(getUser.name, getUser.email)
+    return {
+      userId: user.userId,
+      name: user.name,
+      email: user.email,
+    }
   }
 }
--- tests/integration/show-user.test.ts
+++ tests/integration/show-user.test.ts
@@ -1,11 +1,26 @@
 import { describe, expect, test } from 'bun:test'
 
 import { ShowUserUseCase } from '@/application/usecases'
+import { User } from '@/domain/entities'
+import { UserRepository } from '@/infra/repositories'
 
 describe('INT', () => {
   test('show user', async () => {
-    const usecase = new ShowUserUseCase()
-    const output = await usecase.execute()
-    expect(output).toBe('Hello World!')
+    const repository = new UserRepository()
+    const user1 = new User('john doe', 'johndoe@mail.com')
+    const user2 = new User('john three', 'johntrhee@mail.com')
+    const user3 = new User('john travolta', 'johntravolta@mail.com')
+    repository.database.push(user1)
+    repository.database.push(user2)
+    repository.database.push(user3)
+    const usecase = new ShowUserUseCase(repository)
+    const userId = user3.userId
+    const output = await usecase.execute(userId)
+    // expect(output.userId).toEqual(userId)
+    expect(output).toEqual({
+      userId: expect.any(String),
+      name: 'john travolta',
+      email: 'johntravolta@mail.com',
+    })
   })
 })

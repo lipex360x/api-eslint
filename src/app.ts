57dc571b8d5c75901ab888eb001a081f3b79c1b7
Felipe Bambirra <lipex360@gmail.com>
2024-09-15 12:27:58 +0100

    logs removed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono'
-import { logger } from 'hono/logger'
 
+// import { logger } from 'hono/logger'
 import { baseRoutes } from './infra/routes'
 
 const app = new Hono()
-app.use(logger())
+// app.use(logger())
 
 app.route('', baseRoutes)
 
